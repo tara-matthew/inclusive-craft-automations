67dc783fa7e3a32bc8698d33f64cee42c9d054b6
tara-matthew <taramatthew.93@gmail.com>
2025-11-29 20:28:18 +0000

    style: update form styling

--- resources/views/appointments/create.blade.php
+++ resources/views/appointments/create.blade.php
@@ -73,12 +73,20 @@
     }
     input {
         width: 300px;
+        border-radius: 8px;
+        border-color: #1a202c;
+        border-width: 1px;
+        border-style: solid;
+        padding: 5px;
     }
     .form-container {
         padding: 20px;
-        width:50%;
+        width: 25%;
         border: 1px solid grey;
-        border-radius: 16px
+        border-radius: 16px;
+        margin-left: auto;
+        margin-right: auto;
+        margin-top: 200px;
     }
     .required {
         color: red;
@@ -87,5 +95,16 @@
         color: red;
         position: absolute;
         top: 25px;
+    }
+    button {
+        border-radius: 10px;
+        padding: 10px;
+        border-style: solid;
+        border-width: 1px;
+        width: 100px;
+    }
+    button:hover {
+        cursor: pointer;
+        background-color: gray;
     }
 </style>
