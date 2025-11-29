ac687198d2c97197088a4f5fb7689205ffd7e2de
tara-matthew <taramatthew.93@gmail.com>
2025-11-29 14:42:26 +0000

    test: add test for storing an appointment

---
+++ tests/Feature/StoreAppointmentTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use Carbon\Carbon;
+
+it('stores an appointment and appointment reminder when creating a new customer', function () {
+    Carbon::setTestNow(Carbon::parse('2024-03-31 12:00:00'));
+
+    $response = $this->postJson(route('api.appointments.store'), [
+        'name' => 'Bobby Newport',
+        'email' => 'bobby-newport@hotmail.com',
+        'scheduled_at' => now()->addDays(2)->format('Y-m-d H:i:s'),
+    ])
+        ->assertCreated();
+
+    $appointmentId = $response->json('data.id');
+
+    $this->assertDatabaseHas('customers', [
+        'name' => 'Bobby Newport',
+        'email' => 'bobby-newport@hotmail.com',
+    ]);
+
+    $this->assertDatabaseHas('appointment_reminders', [
+        'appointment_id' => $appointmentId,
+        'send_at' => now()->addDay()->format('Y-m-d H:i:s'),
+    ]);
+});
