4f0bdb3dbd06d21d66b8efacdfd2d2575b414c40
tara-matthew <taramatthew.93@gmail.com>
2026-02-09 19:21:36 +0000

    build: add Maizzle

---
+++ maizzle/config.production.js
@@ -0,0 +1,29 @@
+/*
+|-------------------------------------------------------------------------------
+| Production config                       https://maizzle.com/docs/environments
+|-------------------------------------------------------------------------------
+|
+| This is the production configuration that Maizzle will use when you run the
+| `npm run build` command. Settings here will be merged on top of the base
+| `config.js`, so you only need to add the options that are changing.
+|
+*/
+
+/** @type {import('@maizzle/framework').Config} */
+export default {
+  build: {
+    output: {
+      path: '../resources/views/emails',
+      extension: 'blade.php'
+    },
+    assets: {
+      destination: '../../../public/images/emails'
+    }
+  },
+  css: {
+    inline: true,
+    purge: true,
+    shorthand: true,
+  },
+  prettify: true,
+}
---
+++ resources/views/emails/confirmation.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en" xmlns:v="urn:schemas-microsoft-com:vml">
+<head>
+  <meta charset="utf-8">
+  <meta name="x-apple-disable-message-reformatting">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="format-detection" content="telephone=no, date=no, address=no, email=no, url=no">
+  <meta name="color-scheme" content="light dark">
+  <meta name="supported-color-schemes" content="light dark">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
+  <!--[if mso]>
+  <noscript>
+    <xml>
+      <o:OfficeDocumentSettings xmlns:o="urn:schemas-microsoft-com:office:office">
+        <o:PixelsPerInch>96</o:PixelsPerInch>
+      </o:OfficeDocumentSettings>
+    </xml>
+  </noscript>
+  <style>
+    td,th,div,p,a,h1,h2,h3,h4,h5,h6 {font-family: "Segoe UI", sans-serif; mso-line-height-rule: exactly;}
+  </style>
+  <![endif]-->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap" rel="stylesheet" media="screen">
+  <style>
+    @media (max-width: 600px) {
+      .sm-mx-16px {
+        margin-left: 16px !important;
+        margin-right: 16px !important
+      }
+      .sm-mt-16px {
+        margin-top: 16px !important
+      }
+      .sm-w-361px {
+        width: 361px !important
+      }
+    }
+  </style>
+</head>
+<body style="margin: 0; width: 100%; padding: 0; -webkit-font-smoothing: antialiased; word-break: break-word">
+  <div role="article" aria-roledescription="email" aria-label lang="en">
+    <div>
+      <div style="background-color: #f5f3ee; max-width: 700px; margin-left: auto; margin-right: auto" class="sm-mx-16px sm-mt-16px">
+        <div style="text-align: center; padding-top: 35px; padding-left: 16px; padding-right: 16px">
+          <img class="sm-w-361px" src="https://ci3.googleusercontent.com/meips/ADKq_Na0tf6Gaqmoj9Xs9yA61q4Kc0qLK1YhMy6yAs5oNl6vV0bzZMHs3eFi9RDnMktIE53SdS_vQ5vb8Nw9wpa35SLlMGvedr5DcRD0AzaWM7r6lRFhOvccGiBFBcEgVjNd2jzASHr53iOpbYG7u2llouNySvYGFi5dMy81VYPMXn8j_268stcZ1V9kDqhQpVl_20E6R--scu_7WJjPi_Lf7bK558t0npkIZJWWyYzAsmgaaju2rMbVjQ=s0-d-e1-ft#https://static.wixstatic.com/media/6380cb_db1bf159fbef47ed8df9647aa01714e7~mv2.png/v1/fit/w_448,h_2000,al_c,q_85/6380cb_db1bf159fbef47ed8df9647aa01714e7~mv2.png" alt="logo" style="max-width: 100%; vertical-align: middle; width: 224px" width="224">
+        </div>
+        <div style="padding-left: 30px; padding-right: 30px; text-align: center; font-weight: 600">
+          <p style="color: #656b14; font-family: 'Montserrat', sans-serif; font-weight: 600; letter-spacing: 0.1em; font-size: 46px;">THANKS FOR GETTING IN TOUCH</p>
+        </div>
+        <p style="color: #656b14; font-family: georgia,serif; font-size: 46px;">Your appointment has been confirmed for December 15th.</p>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
