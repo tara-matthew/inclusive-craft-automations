c9f4ba3788b4d975ed5d8a8be29c19f15cb3c446
tara-matthew <taramatthew.93@gmail.com>
2026-02-11 19:15:59 +0000

    feat: update email styling

--- resources/views/emails/confirmation.blade.php
+++ resources/views/emails/confirmation.blade.php
@@ -35,13 +35,19 @@
       .sm-w-361px {
         width: 361px !important
       }
+      .sm-max-w-full {
+        max-width: 100% !important
+      }
+      .sm-p-3 {
+        padding: 12px !important
+      }
     }
   </style>
 </head>
 <body style="margin: 0; width: 100%; padding: 0; -webkit-font-smoothing: antialiased; word-break: break-word">
   <div role="article" aria-roledescription="email" aria-label lang="en">
     <div>
-      <div style="background-color: #f5f3ee; max-width: 700px; margin-left: auto; margin-right: auto; padding: 32px" class="sm-mx-16px sm-mt-16px">
+      <div style="background-color: #f5f3ee; margin-left: auto; margin-right: auto; max-width: 700px; padding: 32px" class="sm-mx-16px sm-mt-16px sm-p-3 sm-max-w-full">
         <div style="text-align: center; padding-top: 35px; padding-left: 16px; padding-right: 16px">
           <img class="sm-w-361px" src="https://ci3.googleusercontent.com/meips/ADKq_Na0tf6Gaqmoj9Xs9yA61q4Kc0qLK1YhMy6yAs5oNl6vV0bzZMHs3eFi9RDnMktIE53SdS_vQ5vb8Nw9wpa35SLlMGvedr5DcRD0AzaWM7r6lRFhOvccGiBFBcEgVjNd2jzASHr53iOpbYG7u2llouNySvYGFi5dMy81VYPMXn8j_268stcZ1V9kDqhQpVl_20E6R--scu_7WJjPi_Lf7bK558t0npkIZJWWyYzAsmgaaju2rMbVjQ=s0-d-e1-ft#https://static.wixstatic.com/media/6380cb_db1bf159fbef47ed8df9647aa01714e7~mv2.png/v1/fit/w_448,h_2000,al_c,q_85/6380cb_db1bf159fbef47ed8df9647aa01714e7~mv2.png" alt="logo" style="max-width: 100%; vertical-align: middle; width: 224px" width="224">
         </div>
