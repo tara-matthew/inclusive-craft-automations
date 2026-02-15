6d00d08d14b193f471ac3c1d434095a34ec1c565
tara-matthew <taramatthew.93@gmail.com>
2026-02-15 17:56:16 +0000

    feat: update StoreAppointmentRequest

--- app/Http/Requests/StoreAppointmentRequest.php
+++ app/Http/Requests/StoreAppointmentRequest.php
@@ -17,8 +17,8 @@ class StoreAppointmentRequest extends FormRequest
         return [
             'name' => ['required', 'string', 'max:255'],
             'secondary_name' => ['nullable', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'scheduled_at' => ['required', 'date'],
+            'email' => ['required', 'string', 'email', 'max:255'],
+            'scheduled_at' => ['required', 'date', 'after:today'],
         ];
     }
 }
