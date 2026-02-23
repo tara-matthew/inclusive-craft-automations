533a388032edcc22cc7fc33a21600f7e716d9b4c
tara-matthew <taramatthew.93@gmail.com>
2026-02-23 22:17:05 +0000

    chore: move Maizzle directory

---
+++ resources/views/emails/maizzle-emails/confirmation.blade.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en" xmlns:v="urn:schemas-microsoft-com:vml">
+<head>
+  <meta charset="utf-8">
+  <meta name="x-apple-disable-message-reformatting">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="format-detection" content="telephone=no, date=no, address=no, email=no, url=no">
+  <meta name="color-scheme" content="light dark">
+  <meta name="supported-color-schemes" content="light dark">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
+  <!--[if mso]>
+  <noscript>
+    <xml>
+      <o:OfficeDocumentSettings xmlns:o="urn:schemas-microsoft-com:office:office">
+        <o:PixelsPerInch>96</o:PixelsPerInch>
+      </o:OfficeDocumentSettings>
+    </xml>
+  </noscript>
+  <style>
+    td,th,div,p,a,h1,h2,h3,h4,h5,h6 {font-family: "Segoe UI", sans-serif; mso-line-height-rule: exactly;}
+  </style>
+  <![endif]-->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap" rel="stylesheet" media="screen">
+  <style>
+    .hover-bg-black:hover {
+      background-color: #000001 !important
+    }
+    .hover-text-white:hover {
+      color: #fffffe !important
+    }
+    @media (max-width: 600px) {
+      .sm-w-200px {
+        width: 200px !important
+      }
+      .sm-max-w-full {
+        max-width: 100% !important
+      }
+      .sm-p-3 {
+        padding: 12px !important
+      }
+    }
+  </style>
+</head>
+<body style="margin: 0; width: 100%; padding: 0; -webkit-font-smoothing: antialiased; word-break: break-word">
+  <div role="article" aria-roledescription="email" aria-label lang="en">
+    <div>
+      <div class="sm-p-3 sm-max-w-full" style="margin-left: auto; margin-right: auto; max-width: 700px; padding: 32px">
+        <div style="padding-left: 16px; padding-right: 16px; text-align: center">
+          <div style="background: linear-gradient(#fff, #fff); margin-left: auto; margin-right: auto; width: 224px; border-radius: 9999px; background-color: #fffffe" class="sm-w-200px">
+            <img class="sm-w-200px" src="https://ci3.googleusercontent.com/meips/ADKq_Na0tf6Gaqmoj9Xs9yA61q4Kc0qLK1YhMy6yAs5oNl6vV0bzZMHs3eFi9RDnMktIE53SdS_vQ5vb8Nw9wpa35SLlMGvedr5DcRD0AzaWM7r6lRFhOvccGiBFBcEgVjNd2jzASHr53iOpbYG7u2llouNySvYGFi5dMy81VYPMXn8j_268stcZ1V9kDqhQpVl_20E6R--scu_7WJjPi_Lf7bK558t0npkIZJWWyYzAsmgaaju2rMbVjQ=s0-d-e1-ft#https://static.wixstatic.com/media/6380cb_db1bf159fbef47ed8df9647aa01714e7~mv2.png/v1/fit/w_448,h_2000,al_c,q_85/6380cb_db1bf159fbef47ed8df9647aa01714e7~mv2.png" alt="logo" style="max-width: 100%; vertical-align: middle; width: 224px" width="224">
+          </div>
+        </div>
+        <div style="padding-left: 12px; padding-right: 12px; text-align: center; font-weight: 500; letter-spacing: 0.1em">
+          <p style="font-family: 'Montserrat', Helvetica, Arial, serif; font-size: 2.875rem; color: #000001">THANKS FOR GETTING IN TOUCH</p>
+        </div>
+        <div style="margin-bottom: 24px; text-align: center">
+          <p style="font-family: 'Montserrat', Helvetica,Arial, serif; font-size: 24px; font-weight: 100; letter-spacing: 0.01em; color: #000001">Your appointment has been confirmed for
+            <br> {{ $appointment-&gt;scheduled_at-&gt;format(&#039;F jS \a\t g:i a&#039;) }}.
+          </p>
+          <p style="font-family: 'Montserrat', Helvetica,Arial, serif; font-size: 16px; font-weight: 100; letter-spacing: 0.01em; color: #000001">
+            Need to reschedule? Email or call to get in touch.
+          </p>
+          <p style="font-family: 'Montserrat', Helvetica,Arial, serif; margin-bottom: 32px; font-size: 16px; font-weight: 100; letter-spacing: 0.01em; color: #000001">
+            Looking for inspiration? Take a look at my website for some ideas.
+          </p>
+          <a style="font-family: 'Montserrat', Helvetica,Arial, serif; cursor: pointer; border: 1px solid #000001; padding: 8px; font-size: 16px; font-weight: 100; letter-spacing: 0.01em; color: #000001; text-decoration: none" href="#" class="hover-bg-black hover-text-white">Go to Site</a>
+        </div>
+        <div style="text-align: center">
+          <a style="margin-right: 8px">
+            <img style="max-width: 100%; vertical-align: middle" width="25" height="25" src="https://img.icons8.com/?size=100&id=85154&format=png&color=3A3A3A" alt="instagram">
+          </a>
+          <a style="margin-right: 8px">
+            <img style="max-width: 100%; vertical-align: middle" width="25" height="25" src="https://img.icons8.com/?size=100&id=118490&format=png&color=3A3A3A" alt="facebook-new">
+          </a>
+          <a style="margin-right: 8px">
+            <img style="max-width: 100%; vertical-align: middle" width="25" height="25" src="https://img.icons8.com/?size=100&id=Uj9DyJeLazL6&format=png&color=3A3A3A" alt="linkedin">
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
