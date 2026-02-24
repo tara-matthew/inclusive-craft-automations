c1c0ac2a20c570b583570a4d242e4003f2f1d2ce
tara-matthew <taramatthew.93@gmail.com>
2026-02-24 12:29:16 +0000

    feat: add a transaction when storing an appointment

--- app/Http/Controllers/StoreAppointmentController.php
+++ app/Http/Controllers/StoreAppointmentController.php
@@ -8,32 +8,36 @@ use App\Models\Appointment;
 use App\Models\AppointmentReminder;
 use App\Models\Customer;
 use App\ReminderStatus;
+use Illuminate\Support\Facades\DB;
 
 class StoreAppointmentController extends Controller
 {
     public function __invoke(StoreAppointmentRequest $request)
     {
-        // TODO use a transaction
         $validated = $request->validated();
 
-        $customer = Customer::firstOrCreate([
-            'name' => $validated['name'],
-            'secondary_name' => $validated['secondary_name'] ?? null,
-            'email' => $validated['email'],
-        ]);
+        $appointment = DB::transaction(function () use ($validated) {
+            $customer = Customer::firstOrCreate([
+                'name' => $validated['name'],
+                'secondary_name' => $validated['secondary_name'] ?? null,
+                'email' => $validated['email'],
+            ]);
 
-        $appointment = new Appointment([
-            'scheduled_at' => $validated['scheduled_at'],
-        ]);
+            $appointment = new Appointment([
+                'scheduled_at' => $validated['scheduled_at'],
+            ]);
 
-        $appointment->customer()->associate($customer)->save();
+            $appointment->customer()->associate($customer)->save();
 
-        $appointmentReminder = new AppointmentReminder([
-            'send_at' => $appointment->scheduled_at->subDays(1),
-            'status' => ReminderStatus::UNPROCESSED,
-        ]);
+            $appointmentReminder = new AppointmentReminder([
+                'send_at' => $appointment->scheduled_at->subDays(1),
+                'status' => ReminderStatus::UNPROCESSED,
+            ]);
 
-        $appointmentReminder->appointment()->associate($appointment)->save();
+            $appointmentReminder->appointment()->associate($appointment)->save();
+
+            return $appointment;
+        });
 
         AppointmentCreated::dispatch($appointment);
 
