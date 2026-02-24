87f1a4954ceb679a80f8cf16bb7865a3fd234098
tara-matthew <taramatthew.93@gmail.com>
2026-02-24 12:29:27 +0000

    test: add test for transaction

--- tests/Feature/StoreAppointmentTest.php
+++ tests/Feature/StoreAppointmentTest.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Models\Appointment;
+use App\Models\AppointmentReminder;
+use App\ReminderStatus;
 use Carbon\Carbon;
 
 it('stores an appointment and appointment reminder when creating a new customer', function () {
@@ -22,4 +24,22 @@ it('stores an appointment and appointment reminder when creating a new customer'
         'appointment_id' => Appointment::first()->id,
         'send_at' => now()->addDay()->format('Y-m-d H:i:s'),
     ]);
+});
+
+it('rolls back updates when part of the request fails', function () {
+    AppointmentReminder::creating(function () {
+        throw new Exception('Server error');
+    });
+
+    $appointmentData = [
+        'name' => 'David Rose',
+        'email' => 'david@hotmail.com',
+        'scheduled_at' => now()->addDays(7)->format('Y-m-d H:i:s'),
+    ];
+
+    $this->post(route('appointments.store'), $appointmentData)->assertServerError();
+
+    $this->assertDatabaseMissing('customers', ['email' => 'john@example.com']);
+    $this->assertDatabaseMissing('appointments', ['scheduled_at' => $appointmentData['scheduled_at']]);
+    $this->assertDatabaseMissing('appointment_reminders', ['status' => ReminderStatus::UNPROCESSED]);
 });
