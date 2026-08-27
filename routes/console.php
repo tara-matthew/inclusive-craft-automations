014dd76db61d1c5d4e5441fc5551057039b3c5c0
tara-matthew <taramatthew.93@gmail.com>
2026-08-27 14:37:02 +0100

    feat: add command to schedule

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+use App\Jobs\SendAppointmentReminders;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::job(new SendAppointmentReminders())->dailyAt('08:00');
