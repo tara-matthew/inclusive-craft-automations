fe9ed9ecb18da7b8f65287539354566591cac68c
tara-matthew <taramatthew.93@gmail.com>
2026-08-28 12:43:52 +0100

    chore: run Pint

--- routes/console.php
+++ routes/console.php
@@ -1,12 +1,12 @@
 <?php
 
+use App\Jobs\SendAppointmentReminders;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
-use App\Jobs\SendAppointmentReminders;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::job(new SendAppointmentReminders())->dailyAt('08:00');
+Schedule::job(new SendAppointmentReminders)->dailyAt('08:00');
