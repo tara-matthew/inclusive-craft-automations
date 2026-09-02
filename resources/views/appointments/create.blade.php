47a9aed026f0914c37c266513e4164442186dcd8
tara-matthew <taramatthew.93@gmail.com>
2026-09-02 18:25:48 +0100

    Revert "style: attempt to prevent datetime overflow on mobile"
    This reverts commit 3e56191a4061d26ec57b3d0aade267d3038919f3.

--- resources/views/appointments/create.blade.php
+++ resources/views/appointments/create.blade.php
@@ -155,9 +155,7 @@
         }
         .form-container {
             flex: 1;
-            min-width: 0;
             max-width: none;
-            overflow-x: hidden;
             border: none;
             border-radius: 0;
             padding: 32px 20px;
